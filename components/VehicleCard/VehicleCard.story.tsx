3e2eddbf09c72fb63c3c29c83eed6f5d67490759
Henry <henrynorth@outlook.com>
2023-06-11 17:32:53 +0100

    build fix

--- components/VehicleCard/VehicleCard.story.tsx
+++ components/VehicleCard/VehicleCard.story.tsx
@@ -4,4 +4,16 @@ export default {
   title: 'Vehicle Card',
 };
 
-export const Usage = () => <VehicleCard />;
+const testVehicle = {
+  id: '1',
+  name: 'Jugular',
+  class: 1,
+  price: '400,000',
+  upgrades: 'Full Upgraded',
+  seats: 4,
+  imageUrl: 'https://static.wikia.nocookie.net/gtawiki/images/9/98/Jugular-GTAO-front.png/revision/latest/scale-to-width-down/350?cb=20190723230439',
+  owner: 'Rex Jagger',
+  ownerPhoneNumber: '92374978234',
+};
+
+export const Usage = () => <VehicleCard {...testVehicle} />;
